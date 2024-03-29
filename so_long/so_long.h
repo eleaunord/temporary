e0fb743037d790a7f829fcfd1571b25e079a5f40
eleaunord <106238878+eleaunord@users.noreply.github.com>
2024-03-29 19:19:30 +0100

    Add files via upload

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,14 +6,14 @@
 /*   By: eleroty <eleroty@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 15:01:04 by eleroty           #+#    #+#             */
-/*   Updated: 2024/03/27 17:47:22 by eleroty          ###   ########.fr       */
+/*   Updated: 2024/03/29 19:18:13 by eleroty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "mlx/mlx.h"
+# include "mlx_linux/mlx.h"
 # include "keys.h"
 # include "libft/libft.h"
 # include "printf/ft_printf.h"
@@ -24,33 +24,64 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}				t_point;
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-typedef struct	s_game {
-	void	*mlx;
-	void	*win;
-    
-}				t_game;
-
-typedef struct	s_map {
-    char **body;
-    int length;
-    int height;
-    int     collectibles;
-    int     exit;
-    int player_pos;
+typedef struct s_map
+{
+	char			**body;
+	int	cols;
+	int	rows;
+	int	num_potions;
+	int	num_exits;
+	int	num_players;
+	int exit;
+	int collectibles;
+	int player_pos;
 }				t_map;
 
-typedef struct	s_images {
+typedef struct s_sprite{
+	void	*wall;
+	int		width;
+	int		height;
+}				t_sprite;
+
+typedef struct s_graphic {
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		height;
+	int		width;
+	int		bpp;
+	int		line_length;
+	int		endian;
+}				t_graphic;
+
+typedef struct s_game
+{
+	t_map			*map;	
+	t_point			curr;
+	t_point			next;
+	t_graphic		graphics;
+	t_sprite		sprite;
+	int	coins;
+	int	moves;
+}				t_game;
 
 
-}				t_images;
+//main.c
+int    exit_error(char *s);
+
+//read_map.c
+void    read_map(char *path, t_game *game);
+
+//check_map.c
+void   check_map(t_game *game);
+
+
 
 #endif
